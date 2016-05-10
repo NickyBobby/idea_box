1ac3152527c4806dd61721b86b03eb71a182564b
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-05-09 22:47:26 -0600

    Add create idea functionality
    Users can now add new ideas to the box without loading the page
    Added AJAX calls add ideas and also to retrieve all the ideas
    Need to style up the index view

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,3 +1,58 @@
 $(document).ready(function() {
-    debugger;
+  getAllIdeas();
+
+  $("#create-idea").click(function() {
+    createIdea();
+  });
 });
+
+function createIdea() {
+  var ideaParams = { idea: { title: $('#title').val(), body: $('#body').val() } };
+  $.ajax({
+    type: 'POST',
+    url:  '/api/v1/ideas',
+    data: ideaParams,
+    success: function(idea) {
+      prependIdea(idea);
+      clearTextFields();
+    },
+    dataType: 'JSON'
+  });
+  console.log("something?");
+}
+
+function getAllIdeas() {
+  $.ajax({
+    type: 'GET',
+    url: '/api/v1/ideas',
+    success: function(ideas) {
+      showIdeasOnPage(ideas);
+    }
+  });
+}
+
+function showIdeasOnPage(ideas) {
+  $.each(ideas, function(index, idea) {
+    addIdea(idea);
+  });
+}
+
+function addIdea(idea) {
+  $('.all-ideas').append(ideaInfo(idea));
+}
+
+function ideaInfo(idea) {
+  return "<div class='idea' id='idea-" + idea.id + "'>" +
+         "<h2>" + idea.title + "</h2>" +
+         "<h3>" + idea.body + "</h3>" +
+         "</div>";
+}
+
+function prependIdea(idea) {
+  $('.all-ideas').prepend(ideaInfo(idea));
+}
+
+function clearTextFields() {
+  $('#title').val('');
+  $('#body').val('');
+}
